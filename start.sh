044dff28b1fe68e614f793b60247206cb65c578a
eprilx <49425492+eprilx@users.noreply.github.com>
2023-03-17 23:43:46 +0700

    Auto rebuild when use docker restart

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
 # Configure Apache2
 echo "ServerName $HOSTNAME" >> /etc/apache2/apache2.conf
 
+cd ${HORIZON_BASEDIR}
+
+# Rebuild
+./manage.py collectstatic --noinput  && ./manage.py compress --force
+
+# Stop services
+service memcached stop
+service apache2 stop
+rm -f /var/run/apache2/apache2.pid
+
 # Start the services
 service memcached start
 /usr/sbin/apache2 -DFOREGROUND
